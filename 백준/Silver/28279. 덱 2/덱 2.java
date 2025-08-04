be5d2a46f6cde766eb586497b8500694d471bcec
dowon <163487251+dowon0113@users.noreply.github.com>
2025-08-04 18:44:06 +0900

    [Silver IV] Title: 덱 2, Time: 1084 ms, Memory: 310728 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -1,41 +1,56 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Deque<Integer> deque = new ArrayDeque<>();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        while (N-- > 0) {
-            String[] cmd = br.readLine().split(" ");
+        Deque<Integer> deque = new ArrayDeque<>();
 
-            switch (cmd[0]) {
-                case "1":
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            String[] cmd = line.split(" ");
+
+            int command = Integer.parseInt(cmd[0]);
+
+            switch (command) {
+                case 1:
                     deque.addFirst(Integer.parseInt(cmd[1]));
                     break;
-                case "2":
+                case 2:
                     deque.addLast(Integer.parseInt(cmd[1]));
                     break;
-                case "3":
-                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+                case 3:
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.pollFirst()).append('\n');
+                    }
                     break;
-                case "4":
-                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+                case 4:
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.pollLast()).append('\n');
+                    }
                     break;
-                case "5":
-                    sb.append(deque.size()).append("\n");
+                case 5:
+                    sb.append(deque.size()).append('\n');
                     break;
-                case "6":
-                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                case 6:
+                    sb.append(deque.isEmpty() ? 1 : 0).append('\n');
                     break;
-                case "7":
-                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+                case 7:
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append('\n');
                     break;
-                case "8":
-                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
+                case 8:
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append('\n');
                     break;
             }
         }

a8eab3f81add5640ee01d81585254195767e9839
dowon <163487251+dowon0113@users.noreply.github.com>
2025-08-20 00:03:29 +0900

    [Silver IV] Title: 듣보잡, Time: 296 ms, Memory: 29080 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,27 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
-            map.put(br.readLine(), 1);
-        }
-        for (int i = 0; i < m; i++) {
-            String name = br.readLine();
-            map.put(name, map.getOrDefault(name,0) +  1);
-            if (map.get(name) == 2) list.add(name);
-        }
         StringBuilder sb = new StringBuilder();
-        Collections.sort(list);
-        sb.append(list.size() + "\n");
-        for (String s : list) {
-            sb.append(s + "\n");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); //듣도 못한 사람 수
+        int M = Integer.parseInt(st.nextToken()); //보도 못한 사람 수
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < N; i++) {
+            String name = br.readLine();
+            map.put(name, 1);
         }
+
+        List<String> result = new ArrayList<>();
+        for(int i = 0; i < M; i++) {
+            String name = br.readLine();
+            map.put(name,map.getOrDefault(name,0) + 1);
+            if(map.get(name) == 2){
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        sb.append(result.size()).append('\n');
+        for(String s : result) {
+            sb.append(s).append('\n');
+        }
+
         System.out.print(sb);
     }
 }
